c7ef23c8208aba80142234f3f9f20289bd3282b2
Philippe Kalitine <philippe.kalitine@gmail.com>
2020-09-14 20:53:37 +0200

    style(): fix all issues

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
 import { FcaService } from './services/fca.service'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [FcaService],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
